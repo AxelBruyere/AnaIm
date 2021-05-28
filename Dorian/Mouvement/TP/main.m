097227dcea1368a916226c4a6bcdd2d34e1fee67
Bregs <dorian.fabregue@hotmail.fr>
2021-05-28 19:20:48 +0200

    Maj

---
+++ Dorian/Mouvement/TP/main.m
@@ -0,0 +1,55 @@
+clc;close all;clear variables;
+
+%%Parametres
+nbr_images = 2; %Nombre d'images a  traiter
+dir = 'RubberWhale'; %Choix des images a  traiter
+method = 'LK'; %Choix de l'algorithme ('HS' pour Horn et Schunk, 
+               %                       'LK' pour Lucas et Kanade)
+
+%Parametres de l'algorithme de Horn et Schunk
+lambda = 1; 
+threshold = 1e-07; %Seuil pour le critere d'arret
+mean_filter = '9-voisinage'; %Choix du filtre moyenneur
+
+%Parametres de l'algorithme de Lucas et Kanade
+n = 25;      %Dimensions du voisinage (matrice n * n, n impair pour centrer le pixel courant)
+mu = 0; sigma = 1; %Parametre de la gaussienne (poids des voisins)
+
+%Parametres d'affichage
+display_step = 6; %Pas d'affichage du champ de vecteurs
+im2display = 2;%Choix de l'image a afficher seule (affiche le mouvement 
+               %entre les images i et i+1)
+
+
+for i = 1:nbr_images-1
+    image1_f = [dir,'/i000',int2str(i),'.png'];  
+    image2_f = [dir,'/i000',int2str(i+1),'.png'];      
+
+%%Estimation du flux
+    switch method
+        case 'HS'
+            [u,v,H,W,I1,it] = motion_hs(image1_f,image2_f,lambda,mean_filter,threshold);
+        case 'LK'
+            [u,v,H,W,I1] = motion_lk(image1_f,image2_f,n,mu,sigma);
+    end
+
+    
+%%Affichage
+    [X,Y] = meshgrid(1:W,1:H);
+    figure(1)
+    subplot(2,fix(nbr_images/2),i)
+    imshow(I1,[])
+    hold on
+    quiver(X(1:display_step:H,1:display_step:W),Y(1:display_step:H,1:display_step:W),...
+        u(1:display_step:H,1:display_step:W), v(1:display_step:H,1:display_step:W),5,'linewidth',0.5)
+    title(sprintf('Flux optique entre les images %d et %d',i,i+1))
+     if i == im2display 
+        figure(10)
+        imshow(I1,[])
+        hold on
+        quiver(X(1:display_step:H,1:display_step:W),Y(1:display_step:H,1:display_step:W),...
+        u(1:display_step:H,1:display_step:W), v(1:display_step:H,1:display_step:W),5,'linewidth',0.5)
+        title(sprintf('Flux optique entre les images %d et %d',i,i+1))
+     end
+end
+   
---
+++ Dorian/Mouvement/TP/motion_lk.m
@@ -0,0 +1,59 @@
+function [u,v,H,L,I1] = motion_lk(image1,image2,n,mu,sigma)
+
+%Parametres d'entree
+%    - image1 -> adresse de la premiere image
+%    - image2 -> adresse de la seconde image
+%    - n -> taille du voisinage considere
+%    - mu,sigma -> parametres de la gaussienne pour le poids des voisins
+    
+%Parametres de sortie
+%   - (u,v) -> le flux optique
+%   - (H,W) -> dimensions des images
+
+%Recuperation des images
+I1 = im2double(imread(image1));
+I2 = im2double(imread(image2));
+[H,L] = size(I1);
+
+%Calcul des gradients
+It = I2-I1;
+[Ix,Iy] = imgradientxy(I1);
+
+%Poids des voisins
+wt = linspace(-1,1,n*n);
+w = 1/(sqrt(2*pi)*sigma)*exp(-(wt-mu).^2/(2*sigma^2));
+W = diag(w);
+W2 = W^2; %Calcul de W^2 hors de la boucle (temps d'execution)
+
+
+%Initialisation de (u,v)
+u = zeros(H,L);
+v = zeros(H,L);
+
+k = n-fix(n/2)-1;%Variable pour le dimensionnement
+count = 0
+for i = 1+k:H-k
+    for j = 1+k:L-k
+        count = count + 1;
+
+        %Vectorisation des gradients (1*n^2)
+        gt = reshape(It(i-k:i+k,j-k:j+k),[],1);
+        gx = reshape(Ix(i-k:i+k,j-k:j+k),[],1);
+        gy = reshape(Iy(i-k:i+k,j-k:j+k),[],1);
+        
+        %Matrices A et b
+        A = [gx,gy];
+        b = -gt;
+        
+        %Matrice M
+        M = [sum((w(:).^2).*(gx(:).^2)),sum((w(:).^2).*gx(:).*gy(:));
+             sum((w(:).^2).*gx(:).*gy(:)),sum((w(:).^2).*(gy(:).^2))];
+         
+        uv = pinv(M)*A'*W2*b;
+        u(i,j) = uv(1);
+        v(i,j) = uv(2);
+        if rem(count,1500) == 0
+            count
+        end
+    end
+end
